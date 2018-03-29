c619944831eb5f337c06cdab2e003f6822110a52
Leop0ld <myungseo.kang@opengallery.co.kr>
2018-03-29 13:53:05 +0900

    React -> Vue 로 바꿈

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # PUBG Tracker
 PUBG 레이팅, 전적 조회 웹사이트
+
+## 기술스택
+
+- Front-End: Vue + Semantic UI
+- Back-End: ???
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+
+For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <label>
+      <input type="text" v-model="nickname" placeholder="닉네임을 입력해주세요" />
+      <input type="submit" value="Submit" v-on:click="get_ratings"/>
+    </label>
+    <p>INPUT: {{ nickname }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data() {
+    return {
+      nickname: '',
+    }
+  },
+  methods: {
+    get_ratings: function (e) {
+      alert(this.nickname)
+    }
+  }
+}
+</script>
+
+<style>
+</style>
