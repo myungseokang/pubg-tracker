690d3597a543c55704b64d81cca263a7a323bdb7
Leop0ld <myungseo.kang@opengallery.co.kr>
2018-04-02 09:34:50 +0900

    response 값을 위한 object 생성

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <template>
-  <div id="app">
-    <label>
-      <input type="text" v-model="nickname" placeholder="닉네임을 입력해주세요" />
-      <input type="submit" value="Submit" v-on:click="get_ratings"/>
-    </label>
-    <div v-if="ok" class="ratingBoard">
-      <h2>{{ nickname }}</h2>
-      <p>server: {{ server }}</p>
+  <div id="app" class="ui container">
+    <div class="ui segment">
+      <label>
+        <div class="ui large input InputNickname">
+          <input type="text" v-model="nickname" placeholder="닉네임을 입력해주세요" />
+        </div>
+        <button class="ui pink large button InputNicknameBtn" v-on:click="get_ratings">검색</button>
+      </label>
+    </div>
+    <div v-if="loaded" class="ratingBoard">
+      <h2>{{ response.nickname }}</h2>
+      <p>server: {{ response.server }}</p>
       <ul>
-        <li v-for="user in users" :key="user.id">
+        <li v-for="user in response.users" :key="user.id">
           {{ user.name }}
         </li>
       </ul>
@@ -22,17 +26,21 @@ export default {
   data() {
     return {
       nickname: '',
-      ok: false,
-      server: '',
-      users: [],
+      loaded: false,
+      response: {
+        nickname: '',
+        server: '',
+        users: [],
+      },
     }
   },
   methods: {
     get_ratings: function (e) {
       this.axios.get('https://jsonplaceholder.typicode.com/users').then((response) => {
-        this.ok = true
-        this.server = 'AS'
-        this.users = response.data
+        this.response.nickname = this.nickname
+        this.loaded = true
+        this.response.server = 'AS'
+        this.response.users = response.data
         console.log(response.data)
       })
     }
@@ -41,4 +49,14 @@ export default {
 </script>
 
 <style>
+.InputNickname {
+  width: 80%;
+  box-sizing: border-box;
+  margin: 0 0;
+}
+.InputNicknameBtn {
+  width: 19%;
+  box-sizing: border-box;
+  margin: 0 0;
+}
 </style>
